fd70cea2dcf42128fcd37a46f130f604844f662c
Jose Miguel Chaves Miranda <josemcm85@hotmail.com>
2019-03-15 23:30:05 -0600

    Historia de Usurio 'Agregar Usuarios'

---
+++ SmartPick/FormUserInfo.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormUserInfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormUserInfo))
+        Me.BunifuElipse1 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.BtnUser = New Bunifu.Framework.UI.BunifuThinButton2()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBoxIdUser = New System.Windows.Forms.TextBox()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxLastN = New System.Windows.Forms.TextBox()
+        Me.TextBoxUserName = New System.Windows.Forms.TextBox()
+        Me.TextBoxPass = New System.Windows.Forms.TextBox()
+        Me.TextBoxtitle = New System.Windows.Forms.TextBox()
+        Me.TextBoxemail = New System.Windows.Forms.TextBox()
+        Me.BunifuElipse2 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BtnCancel = New Bunifu.Framework.UI.BunifuThinButton2()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BunifuElipse1
+        '
+        Me.BunifuElipse1.ElipseRadius = 20
+        Me.BunifuElipse1.TargetControl = Me
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Snow
+        Me.Panel1.Controls.Add(Me.TextBoxemail)
+        Me.Panel1.Controls.Add(Me.TextBoxtitle)
+        Me.Panel1.Controls.Add(Me.TextBoxPass)
+        Me.Panel1.Controls.Add(Me.TextBoxUserName)
+        Me.Panel1.Controls.Add(Me.TextBoxLastN)
+        Me.Panel1.Controls.Add(Me.TextBoxName)
+        Me.Panel1.Controls.Add(Me.TextBoxIdUser)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.lbl1)
+        Me.Panel1.Controls.Add(Me.BtnUser)
+        Me.Panel1.Controls.Add(Me.BtnCancel)
+        Me.Panel1.Location = New System.Drawing.Point(25, 22)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(382, 401)
+        Me.Panel1.TabIndex = 0
+        '
+        'BtnUser
+        '
+        Me.BtnUser.ActiveBorderThickness = 1
+        Me.BtnUser.ActiveCornerRadius = 7
+        Me.BtnUser.ActiveFillColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.BtnUser.ActiveForecolor = System.Drawing.Color.White
+        Me.BtnUser.ActiveLineColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.BtnUser.BackColor = System.Drawing.Color.Snow
+        Me.BtnUser.BackgroundImage = CType(resources.GetObject("BtnUser.BackgroundImage"), System.Drawing.Image)
+        Me.BtnUser.ButtonText = "Agregar Usuario"
+        Me.BtnUser.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnUser.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnUser.ForeColor = System.Drawing.Color.SeaGreen
+        Me.BtnUser.IdleBorderThickness = 1
+        Me.BtnUser.IdleCornerRadius = 7
+        Me.BtnUser.IdleFillColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.BtnUser.IdleForecolor = System.Drawing.Color.White
+        Me.BtnUser.IdleLineColor = System.Drawing.Color.Lavender
+        Me.BtnUser.Location = New System.Drawing.Point(220, 327)
+        Me.BtnUser.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnUser.Name = "BtnUser"
+        Me.BtnUser.Size = New System.Drawing.Size(134, 50)
+        Me.BtnUser.TabIndex = 55
+        Me.BtnUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.lbl1.Location = New System.Drawing.Point(54, 29)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(73, 16)
+        Me.lbl1.TabIndex = 56
+        Me.lbl1.Text = "Id Usuario"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(67, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 16)
+        Me.Label1.TabIndex = 57
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(57, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 16)
+        Me.Label2.TabIndex = 58
+        Me.Label2.Text = "Apellidos"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(71, 147)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 16)
+        Me.Label3.TabIndex = 59
+        Me.Label3.Text = "Usuario"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(44, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 16)
+        Me.Label4.TabIndex = 60
+        Me.Label4.Text = "Contraseña"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label5.Location = New System.Drawing.Point(75, 263)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 16)
+        Me.Label5.TabIndex = 62
+        Me.Label5.Text = "Correo"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label6.Location = New System.Drawing.Point(78, 225)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 16)
+        Me.Label6.TabIndex = 61
+        Me.Label6.Text = "Puesto"
+        '
+        'TextBoxIdUser
+        '
+        Me.TextBoxIdUser.Location = New System.Drawing.Point(133, 29)
+        Me.TextBoxIdUser.Name = "TextBoxIdUser"
+        Me.TextBoxIdUser.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxIdUser.TabIndex = 63
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(133, 68)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxName.TabIndex = 64
+        '
+        'TextBoxLastN
+        '
+        Me.TextBoxLastN.Location = New System.Drawing.Point(133, 107)
+        Me.TextBoxLastN.Name = "TextBoxLastN"
+        Me.TextBoxLastN.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxLastN.TabIndex = 65
+        '
+        'TextBoxUserName
+        '
+        Me.TextBoxUserName.Location = New System.Drawing.Point(133, 146)
+        Me.TextBoxUserName.Name = "TextBoxUserName"
+        Me.TextBoxUserName.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxUserName.TabIndex = 66
+        '
+        'TextBoxPass
+        '
+        Me.TextBoxPass.Location = New System.Drawing.Point(133, 185)
+        Me.TextBoxPass.Name = "TextBoxPass"
+        Me.TextBoxPass.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxPass.TabIndex = 67
+        '
+        'TextBoxtitle
+        '
+        Me.TextBoxtitle.Location = New System.Drawing.Point(133, 224)
+        Me.TextBoxtitle.Name = "TextBoxtitle"
+        Me.TextBoxtitle.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxtitle.TabIndex = 68
+        '
+        'TextBoxemail
+        '
+        Me.TextBoxemail.Location = New System.Drawing.Point(133, 263)
+        Me.TextBoxemail.Name = "TextBoxemail"
+        Me.TextBoxemail.Size = New System.Drawing.Size(210, 20)
+        Me.TextBoxemail.TabIndex = 69
+        '
+        'BunifuElipse2
+        '
+        Me.BunifuElipse2.ElipseRadius = 20
+        Me.BunifuElipse2.TargetControl = Me.Panel1
+        '
+        'BtnCancel
+        '
+        Me.BtnCancel.ActiveBorderThickness = 1
+        Me.BtnCancel.ActiveCornerRadius = 7
+        Me.BtnCancel.ActiveFillColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.BtnCancel.ActiveForecolor = System.Drawing.Color.White
+        Me.BtnCancel.ActiveLineColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.BtnCancel.BackColor = System.Drawing.Color.Snow
+        Me.BtnCancel.BackgroundImage = CType(resources.GetObject("BtnCancel.BackgroundImage"), System.Drawing.Image)
+        Me.BtnCancel.ButtonText = "Cerrar"
+        Me.BtnCancel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnCancel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCancel.ForeColor = System.Drawing.Color.SeaGreen
+        Me.BtnCancel.IdleBorderThickness = 1
+        Me.BtnCancel.IdleCornerRadius = 7
+        Me.BtnCancel.IdleFillColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.BtnCancel.IdleForecolor = System.Drawing.Color.White
+        Me.BtnCancel.IdleLineColor = System.Drawing.Color.Lavender
+        Me.BtnCancel.Location = New System.Drawing.Point(18, 327)
+        Me.BtnCancel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnCancel.Name = "BtnCancel"
+        Me.BtnCancel.Size = New System.Drawing.Size(134, 50)
+        Me.BtnCancel.TabIndex = 54
+        Me.BtnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FormUserInfo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.SmartPick.My.Resources.Resources.MANDALAS21
+        Me.ClientSize = New System.Drawing.Size(438, 442)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FormUserInfo"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FormUserInfo"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BunifuElipse1 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents BtnUser As Bunifu.Framework.UI.BunifuThinButton2
+    Friend WithEvents TextBoxemail As TextBox
+    Friend WithEvents TextBoxtitle As TextBox
+    Friend WithEvents TextBoxPass As TextBox
+    Friend WithEvents TextBoxUserName As TextBox
+    Friend WithEvents TextBoxLastN As TextBox
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxIdUser As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents BunifuElipse2 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BtnCancel As Bunifu.Framework.UI.BunifuThinButton2
+End Class
